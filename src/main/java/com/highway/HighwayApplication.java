dd6415b3e20fd2eb40bf26da0be30cfc6ff501e1
598455803 <598455803@qq.com>
2018-10-29 19:59:54 +0800

    swagger

--- src/main/java/com/highway/HighwayApplication.java
+++ src/main/java/com/highway/HighwayApplication.java
@@ -4,6 +4,7 @@ import com.highway.mapper.CommonMapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Creaed by fj on 2018/10/29
--- src/main/java/com/highway/inter/NewsInter.java
+++ src/main/java/com/highway/inter/NewsInter.java
@@ -4,6 +4,8 @@ import com.highway.exception.BaseException;
 import com.highway.model.News;
 import com.highway.service.INewsService;
 import com.highway.util.response.Page;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RequestMapping(value = "newsInter")
 @RestController
+@Api(value = "新闻相关接口")
 public class NewsInter {
 
     @Autowired
@@ -28,6 +31,7 @@ public class NewsInter {
      * @return
      */
     @GetMapping(value="getNewsByType")
+    @ApiOperation(value="根据type和是否发布分页查询新闻", notes="0动态通知 1集团新闻 2媒体报道")
     public Page<News> getNewsByType(Integer pageNo, Integer pageSize, Integer type,boolean isPush) throws BaseException {
         return  newsService.getNewsByType(pageNo,pageSize,type,isPush);
     }
@@ -39,6 +43,7 @@ public class NewsInter {
      * @return
      */
     @GetMapping(value = "getNewsDetail")
+    @ApiOperation(value="根据id查询新闻详情")
     public News getNewsDetail(Integer id){
         return newsService.getNewsDetail(id);
     }
