3c861c9814ba7135ea922652549f7543bebfffda
liuchen0329 <595961662@qq.com>
2018-10-31 10:25:19 +0800

    add partyBuild program

--- src/main/java/com/highway/service/impl/PartyBuildServiceImpl.java
+++ src/main/java/com/highway/service/impl/PartyBuildServiceImpl.java
@@ -1,16 +1,29 @@
 package com.highway.service.impl;
 
+import com.highway.mapper.PartyBuildMapper;
 import com.highway.model.PartyBuild;
 import com.highway.service.PartyBuildService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by Liu Chen on 2018/10/30.
  */
 @Service
+@Slf4j
 public class PartyBuildServiceImpl implements PartyBuildService {
+
+    @Autowired
+    private PartyBuildMapper partyBuildMapper;
+
     @Override
     public Integer savePartyBuildActivity(PartyBuild partyBuild) {
-        return null;
+
+        log.info("partyBuildService add new partyBuild activity,title[{}]",partyBuild.getTitle());
+
+        return partyBuildMapper.insertSelective(partyBuild);
     }
+
+
 }
